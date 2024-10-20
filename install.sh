1c9c66358c7512ec06a4d11bc894111e80561ffd
Atsushi Inutsuka <sekainoinuatsu@gmail.com>
2024-10-21 02:45:56 +0900

    chore: Divide install.sh to link.sh and install.sh

--- install.sh
+++ install.sh
@@ -17,26 +17,6 @@ esac
 
 sh -c "$(curl -sS https://starship.rs/install.sh)" -y -f
 
-IGNORE_PATTERN="^\.(git)"
-
-echo "Creating dotfile links..."
-for dotfile in .??*; do
-  [[ ${dotfile} =~ $IGNORE_PATTERN ]] && continue
-  target="$HOME/${dotfile}"
-
-  if [ -d "$(pwd)/${dotfile}" ]; then
-    mkdir -p "$target"
-
-    for subfile in "$(pwd)/${dotfile}"/*; do
-      subfile_name=$(basename "$subfile")
-      ln -snfv "$subfile" "$target/$subfile_name"
-    done
-  else
-    ln -snfv "$(pwd)/${dotfile}" "$target"
-  fi
-done
-echo "Dotfile links created."
-
 curl https://mise.run | sh
 eval "$(~/.local/bin/mise activate bash)"
 export PATH="$HOME/.local/share/mise/shims:$PATH"
