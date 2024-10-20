c9926b8e11390183202b11bcd0e8f72bf0602a6c
Atsushi Inutsuka <sekainoinuatsu@gmail.com>
2024-10-21 02:05:32 +0900

    fix: Fix how to create symlinks

--- install.sh
+++ install.sh
@@ -21,8 +21,19 @@ IGNORE_PATTERN="^\.(git)"
 
 echo "Creating dotfile links..."
 for dotfile in .??*; do
-  [[ $dotfile =~ $IGNORE_PATTERN ]] && continue
-  ln -snfv "$(pwd)/$dotfile" "$HOME/$dotfile"
+  [[ ${dotfile} =~ $IGNORE_PATTERN ]] && continue
+  target="$HOME/${dotfile}"
+
+  if [ -d "$(pwd)/${dotfile}" ]; then
+    mkdir -p "$target"
+
+    for subfile in "$(pwd)/${dotfile}"/*; do
+      subfile_name=$(basename "$subfile")
+      ln -snfv "$subfile" "$target/$subfile_name"
+    done
+  else
+    ln -snfv "$(pwd)/${dotfile}" "$target"
+  fi
 done
 echo "Dotfile links created."
 
